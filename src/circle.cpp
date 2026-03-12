666d9e2a4cb817c245102da4db895536e20d341c
Kirill <remizov.kirill2005@yandex.ru>
2026-03-12 21:20:54 +0300

    feat(circle): Added realization of circle methods

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,59 @@
 // Copyright 2022 UNN-CS
-#include <cstdint>
 #include "circle.h"
+#include <cmath>
+#include <stdexcept>
 
+const double PI = 3.14159265358979323846;
+
+Circle::Circle(double radius) {
+  if (radius <= 0) {
+    throw std::invalid_argument("Radius must be positive");
+  }
+  radius_ = radius;
+  updateFromRadius();
+}
+
+void Circle::setRadius(double radius) {
+  if (radius <= 0) {
+    throw std::invalid_argument("Radius must be positive");
+  }
+  radius_ = radius;
+  updateFromRadius();
+}
+
+void Circle::setFerence(double ference) {
+  if (ference <= 0) {
+    throw std::invalid_argument("Circumference must be positive");
+  }
+  ference_ = ference;
+  updateFromFerence();
+}
+
+void Circle::setArea(double area) {
+  if (area <= 0) {
+    throw std::invalid_argument("Area must be positive");
+  }
+  area_ = area;
+  updateFromArea();
+}
+
+double Circle::getRadius() const { return radius_; }
+
+double Circle::getFerence() const { return ference_; }
+
+double Circle::getArea() const { return area_; }
+
+void Circle::updateFromRadius() {
+  ference_ = 2 * PI * radius_;
+  area_ = PI * radius_ * radius_;
+}
+
+void Circle::updateFromFerence() {
+  radius_ = ference_ / (2 * PI);
+  area_ = PI * radius_ * radius_;
+}
+
+void Circle::updateFromArea() {
+  radius_ = std::sqrt(area_ / PI);
+  ference_ = 2 * PI * radius_;
+}
